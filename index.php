33ea494202420a0dde2635135a44342160eff923
vvcqv <97289648+vvcqv@users.noreply.github.com>
2022-12-04 18:28:05 +0300

    Updated index.php to new version

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
     <link rel="manifest" href="images/site.webmanifest">
-    <title>Главная страница</title>
+    <title>Главная страница 2</title>
 </head>
 
 <body>
